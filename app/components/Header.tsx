0eee858233c4da06b417eaab590040f912f8f812
FakuKostasDvlpr <rollinsh91@gmail.com>
2024-02-28 15:28:56 -0300

    change

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,5 +1,6 @@
 import { Onest } from "next/font/google"
 import { Poppins } from "next/font/google";
+import Image from "next/image";
 
 const onest = Onest({ subsets: ['latin'], weight: ['900', '600', '700', '500', '400'] });
 const poppins = Poppins({ subsets: ['latin'], weight: ['900', '600', '700', '500', '400'] });
@@ -12,7 +13,7 @@ export default function Header() {
         <>
       <header className="flex m-auto w-full max-w-[1124px] items-center justify-between pt-4 ">
             <div className="text-center flex items-center justify-center gap-4">
-                <img src="https://upload.wikimedia.org/wikipedia/commons/b/b7/Purple_Circle.png" width={30} height={30} alt="logo" />
+                <Image src="https://upload.wikimedia.org/wikipedia/commons/b/b7/Purple_Circle.png" width={30} height={30} alt="logo" />
                 <p className= {`${onest.className} font-bold text-3xl`}>Kostas</p>
             </div>
                 <nav>
