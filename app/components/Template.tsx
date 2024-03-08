2eed3e21d448ddd3c4c51833ee47737a3e09c8d6
FakuKostasDvlpr <rollinsh91@gmail.com>
2024-03-08 18:54:06 -0300

    g

--- app/components/Template.tsx
+++ app/components/Template.tsx
@@ -3,29 +3,29 @@ import ScrollReveal from 'scrollreveal';
 import { data } from "./data";
 import Button from "./Buttons/Button";
 import { AnimatedTooltipPreview } from "./Buttons/Social";
+import Image from "next/image";
 
 export default function Template() {
   useEffect(() => {
-    if (typeof window !== 'undefined') {
-      ScrollReveal().reveal('.reveal-right', {
-        delay: 300,
-        distance: '50px',
-        duration: 2500,
-        easing: 'cubic-bezier(0.5, 0, 0, 1)',
-        origin: 'right',
-        reset: true,
-      });
+    // Configuración para la animación desde la derecha
+    ScrollReveal().reveal('.reveal-right', {
+      delay: 300,
+      distance: '50px',
+      duration: 2500,
+      easing: 'cubic-bezier(0.5, 0, 0, 1)',
+      origin: 'right',
+      reset: true,
+    });
 
-      // Configuración para la animación desde abajo
-      ScrollReveal().reveal('.reveal-bottom', {
-        delay: 300,
-        distance: '50px',
-        duration: 1000,
-        easing: 'cubic-bezier(0.5, 0, 0, 1)',
-        origin: 'bottom',
-        reset: true,
-      });
-    }
+    // Configuración para la animación desde abajo
+    ScrollReveal().reveal('.reveal-bottom', {
+      delay: 300,
+      distance: '50px',
+      duration: 1000,
+      easing: 'cubic-bezier(0.5, 0, 0, 1)',
+      origin: 'bottom',
+      reset: true,
+    });
   }, []);
 
   return (
@@ -47,7 +47,7 @@ export default function Template() {
             )}
             {i === 1 && (
               <div className="flex items-center justify-center w-full h-full reveal-bottom">
-                <img className="rounded-xl object-cover md:w-[640px]" src="/myIcon.png" width={700} height={700} alt="icono"/>
+                <Image className="rounded-xl object-cover md:w-[640px]" src="/myIcon.png" width={700} height={700} alt="icono"/>
               </div>
             )}
           </div>
