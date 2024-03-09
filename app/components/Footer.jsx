8129ba605149de4f47b0dd5862f07fb60359475d
FakuKostasDvlpr <rollinsh91@gmail.com>
2024-03-09 16:12:34 -0300

    up

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -86,8 +86,8 @@ export default function Footer() {
     return (
         <>
         <RevealWrapper delay={300} distance="50px" duration={1000} easing='cubic-bezier(0.5, 0, 0, 1)' origin="bottom" reset={true}> 
-        <footer id="contact" className="reveal-bottom FooterLast rounded-xl mx-auto bg-white  gap-10 p-3 mt-20 mb-6 h-[600px] w-full max-w-[370px] md:max-w-[940px] items-center justify-center">
-            <div className="text-4xl mt-10 mx-4 font-medium">
+        <footer id="contact" className="reveal-bottom FooterLast rounded-xl mx-auto bg-white  gap-10 mb-6 h-[450px] w-full max-w-[370px] md:max-w-[930px] items-center justify-center">
+            <div className="text-4xl pt-6 mx-4 font-medium">
                 <h3>Want to work  <span className="text-[#e3c448] font-medium">together?</span></h3>
             </div>
             <div className="gap-6 flex mt-6 flex-col justify-center items-center text-center">
